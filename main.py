d90219136fae8eccf681f22c32e631cb9b858070
Daniel James <86825914+attlka@users.noreply.github.com>
2021-12-12 11:06:33 +0000

    Added GPTIII token model parameters

--- main.py
+++ main.py
@@ -29,59 +29,38 @@ async def on_ready():
 #bot answer scripting
 @bot.event
 async def on_message(message):
-    #Avoiding the bot to answer itself
+    #Prevents bot from answering own message as prompt
     if message.author == bot.user:
         return
 
-    #Getting the message of the user
+    #Tokenize user message
     user_message = message.content
-
-    #Getting the answer of the user's message with GPT-3 model and tokenizer
     answer = generate_answer(user_message)
-
-    #If there is no answer, we send a default one (sorry I don't understand)
     if answer == '':
         await message.channel.send('Sorry, I don\'t understand.')
-
-    #Else, we send the answer of the user's message with GPT-3 model and tokenizer
     else:
         await message.channel.send(answer)
 
 
-#Function that generates an answer from a user's message with GPT-3 model and tokenizer
+#Answering function
 def generate_answer(user_message):
 
-    #If there is no user's message, we return an empty string (no answer) 
     if user_message == '':
-        return ''
+        return '' 
 
-    #Else, we generate an answer from a user's message with GPT-3 model and tokenizer 
     else:
 
         #Encodes the user's message with GPT-3 tokenizer 
         input_ids = tokenizer.encode(user_message + tokenizer.eos_token, return_tensors='pt')
-
-        #Set a max length for the generated text 
         max_length = 40
-
-        #Sets a minimum length for the generated text 
         min_length = 15
-
-        #Sets a temperature for our text generation process 
         temperature = 0.7
+        top_k = 0  
+        top_p = 0.9 
+        repetition_penalty = 1.0
+        num_return_sequences = 1
 
-        #Sets a top k or top p value for our text generation process 
-        top_k = 0       #If top k is 0, we generate all words of our text generation process 
-
-        top_p = 0.9     #If top p is 1, we generate all words of our text generation process 
-
-        repetition_penalty = 1.0   #If repetition penalty is 1, we don't take into account repeated words in our text generation process 
-
-        num_return_sequences = 1   #If num return sequences is 1, we only get one sentence as output (we don't want to get several sentences as output) 
-
-        bad_words_ids = [tokenizer.encode(badword) for badword in ['idiot', 'stupid', 'shut up']]   #List of bad words that we want to avoid in our text generation process 
-
-      	#Generating an answer from a user's message with GPT-3 model and tokenizer  
+      	#Answer generation 
         outputs = model.generate(input_ids=input_ids, max_length=max_length, min_length=min_length, temperature=temperature, top_k=top_k, top_p=top_p, repetition_penalty=repetition_penalty, num_return_sequences=num_return_sequences, bad_words_ids=bad_words_ids)
 
         #Decoding the answer with GPT-3 tokenizer 
@@ -89,9 +68,6 @@ def generate_answer(user_message):
 
         #Returning the answer 
         return answer
-
-
-
 
 #Running the bot
 bot.run(TOKEN)
